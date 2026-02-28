04640950a0521fd613a899a8d9d196fe94cf43f6
Thybigfoot <henrycfhawkins@gmail.com>
2026-02-28 23:02:36 +0000

    scales dev
    Visual studio is pissing me off rn, gonna delete and redownload

---
+++ On the scale(inside the box)/Assets/Scripts/Scales_gravity.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Scales_gravity : MonoBehaviour
+{
+
+    public Rigidbody2D ScaleA;
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+        
+    
+}
