d04dd8d6358c827c078bcc4bcec92c418a45eb6d
Thybigfoot <henrycfhawkins@gmail.com>
2026-03-10 08:09:31 +0000

    Scene changes
    All scene changes added for main menu to level 1and level 2 and can be reused.
    Can now begin on level design!!
    xoxo
    thank you youtubee
    need no claude dont need no need no claude don't need no need no claude dont need no need no claude

--- On the scale(inside the box)/Assets/Scripts/Door.cs
+++ On the scale(inside the box)/Assets/Scripts/Door.cs
@@ -1,13 +1,29 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
     public Key key;
+    private int index;
+    private int nextIndex;
+    private void Start()
+    {
+        index = SceneManager.GetActiveScene().buildIndex;
+        nextIndex = index + 1;
+    }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && key.hasKey)
         {
-            //change scene
+            if (nextIndex < SceneManager.sceneCountInBuildSettings)
+            {
+                SceneManager.LoadScene(nextIndex);
+            }
+            else
+            {
+                Debug.LogError($"Scene does not existc{nextIndex}");
+            }
         }
     }
 }
